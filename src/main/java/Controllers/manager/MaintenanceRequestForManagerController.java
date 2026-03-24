2bc961c4b7e20137d0222d505d4106aa61973f83
Khangthce <Khangthce190729@gmail.com>
2026-03-24 15:39:06 +0700

    KhangTH: Override

--- src/main/java/Controllers/manager/MaintenanceRequestForManagerController.java
+++ src/main/java/Controllers/manager/MaintenanceRequestForManagerController.java
@@ -20,7 +20,7 @@ import jakarta.servlet.http.HttpServletResponse;
  * @author truon
  */
 public class MaintenanceRequestForManagerController extends HttpServlet {
-
+@Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
